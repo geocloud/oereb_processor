4154490de9959bc4f4683a5838871b5e4723aa5b
Daniel Baumgartner <daniel.baumgartner@geocloud.ch>
2017-03-20 13:03:03 +0100

    add gitignore

---
+++ init.bat
@@ -0,0 +1,7 @@
+echo "# oereb_processor" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/geocloud/oereb_processor.git
+git push -u origin master
+pause
